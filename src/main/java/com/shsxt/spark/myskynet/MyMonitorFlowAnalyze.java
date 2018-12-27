05edc592f2fada3d6ba48595aaf62bcaf620817a
MonkeyOnFire <897382832@qq.com>
2018-12-27 09:21:34 +0800

    monitor

--- src/main/java/com/shsxt/spark/myskynet/MyMonitorFlowAnalyze.java
+++ src/main/java/com/shsxt/spark/myskynet/MyMonitorFlowAnalyze.java
@@ -1,16 +1,16 @@
 package com.shsxt.spark.myskynet;
 
+import com.google.common.base.Optional;
+import org.apache.spark.Accumulator;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.api.java.function.VoidFunction;
 import scala.Tuple2;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by Iskandar on 2018/12/25.
@@ -75,8 +75,58 @@ public class MyMonitorFlowAnalyze {
         }).sortByKey(false);
 
         List<Tuple2<Integer,String>> list = rddA5.take(5);
+        int i = 1;
         for (Tuple2<Integer,String> t : list){
-            System.out.println(t._2()+t._1());
+            System.out.println("流量第"+i+"的卡口为"+ t._2() +"，车流量为："+ t._1());
+            i++;
         }
+
+        JavaRDD<String> rddB1 = sparkContext.textFile("data/monitor_camera_info");
+        JavaPairRDD<String,String> rddB2 = rddB1.mapToPair(new PairFunction<String, String, String>() {
+            private static final long serialVersionUID = 1L;
+            @Override
+            public Tuple2<String, String> call(String s) throws Exception {
+                String[] ss = s.split("\t");
+                return new Tuple2<>(ss[0],ss[1]);
+            }
+        });
+        JavaPairRDD<String,Iterable<String>> rddB3 = rddB2.groupByKey();
+
+        JavaPairRDD<String,String> rddB4 = rddB3.mapToPair(new PairFunction<Tuple2<String, Iterable<String>>, String, String>() {
+            private static final long serialVersionUID = 1L;
+            @Override
+            public Tuple2<String, String> call(Tuple2<String, Iterable<String>> t) throws Exception {
+                Iterator<String> iterator = t._2.iterator();
+                int cameraCount = 0;
+                String cameraInfo = "";
+                while (iterator.hasNext()){
+                    String s = iterator.next();
+                    cameraInfo = cameraInfo + "摄像头：" + s + "，";
+                    cameraCount++;
+                }
+                cameraInfo = cameraInfo.substring(0,cameraInfo.length()-1);
+                return new Tuple2<>(t._1,cameraInfo+"|"+cameraCount);
+            }
+        });
+
+        Accumulator<Integer> errMonitor = sparkContext.accumulator(0);
+        Accumulator<Integer> nomalMonitor = sparkContext.accumulator(0);
+        Accumulator<Integer> errCamera = sparkContext.accumulator(0);
+        Accumulator<Integer> nomalCamera = sparkContext.accumulator(0);
+        JavaPairRDD<String,Tuple2<String,Optional<String>>> joinRdd = rddB4.leftOuterJoin(rddA4);
+        joinRdd.foreach(new VoidFunction<Tuple2<String, Tuple2<String, Optional<String>>>>() {
+            @Override
+            public void call(Tuple2<String, Tuple2<String, Optional<String>>> t) throws Exception {
+                Optional o = t._2._2;
+                
+            }
+        });
+
+
+
+
+
+
+
     }
 }
